82e4333d4527c680cabac464e4ef176d54220b26
hwalia <hemant.walia1988@gmail.com>
2025-12-11 22:39:22 +0000

    committing new bike model.

--- EXPLORE/bike.sql
+++ EXPLORE/bike.sql
@@ -20,41 +20,52 @@ select
     ,t.$11
     ,t.$12
     ,t.$13
+    ,t.$14
+    ,t.$15
 FROM
-    @DEMO.DEMO_SCHEMA.BIKE_STAGE (FILE_FORMAT => 'CSVBIKE') t;
+    @DEMO.DEMO_SCHEMA.BIKE_STAGE (FILE_FORMAT => 'CSVBIKE') t limit 100;
 
-		CREATE OR REPLACE TABLE DEMO.DEMO_SCHEMA.BIKE (
 
-		RIDE_ID STRING,
-		RIDEABLE_TYPE STRING,
-		STARTED_AT STRING,
-		ENDED_AT STRING,
-		START_STATION_NAME STRING,
-		START_STATIO_ID STRING,
-		END_STATION_NAME STRING,
-		END_STATION_ID STRING,
-		START_LAT STRING,
-		START_LNG STRING,
-		END_LAT STRING,
-		END_LNG STRING,
-		MEMBER_CSUAL STRING);
+CREATE OR REPLACE TABLE DEMO.DEMO_SCHEMA.BIKE 
+(
+    TRIPDURATION STRING
+    ,STARTTIME STRING
+    ,STOPTIME STRING	
+    ,START_STATION_ID STRING	
+    ,START_STATION_NAME STRING	
+    ,START_STATION_LATITUDE STRING	
+    ,START_STATION_LONGITUDE STRING	
+    ,END_STATION_ID STRING	
+    ,END_STATION_NAME STRING	
+    ,END_STATION_LATITUDE STRING	
+    ,END_STATION_LONGITUDE STRING	
+    ,BIKEID STRING	
+    ,USERTYPE STRING	
+    ,BIRTH_YEAR STRING	
+    ,GENDER STRING
+);
 
-	COPY INTO DEMO.DEMO_SCHEMA.BIKE
-			FROM
-			(
-			SELECT
-			t.$1,
-			t.$2,
-			t.$3,
-			t.$4,
-			t.$5,
-			t.$6,
-			t.$7,
-			t.$8,
-			t.$9,
-			t.$10,
-			t.$11,
-			t.$12,
-			t.$13
-			FROM
-			@DEMO_SCHEMA.BIKE_STAGE t);
+        
+
+COPY INTO DEMO.DEMO_SCHEMA.BIKE
+FROM
+(
+    SELECT
+        t.$1,
+        t.$2,
+        t.$3,
+        t.$4,
+        t.$5,
+        t.$6,
+        t.$7,
+        t.$8,
+        t.$9,
+        t.$10,
+        t.$11,
+        t.$12,
+        t.$13,
+        t.$14,
+        t.$15
+    FROM
+    @DEMO_SCHEMA.BIKE_STAGE t
+);
